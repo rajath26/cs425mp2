94e019e49e2ff8022aff9dc3fc880419b0c4291f
grkrthk <grkrthk@yahoo.co.in>
2013-10-04 21:27:27 -0500

    random number generation

--- src/udp.c
+++ src/udp.c
@@ -85,16 +85,56 @@ int sendUDP(int portNo, char * ipAddr, char * buffer)
     int numOfBytesSent;                 // Number of bytes sent 
 
     struct sockaddr_in hostAddr;        // Address of host to send message
-
+    int value;
     memset(&hostAddr, 0, sizeof(struct sockaddr_in));
     hostAddr.sin_family = AF_INET;
     hostAddr.sin_port = htons(portNo);
     hostAddr.sin_addr.s_addr = inet_addr(ipAddr);
     memset(&(hostAddr.sin_zero), '\0', 8);
+    static int k=-1;
+    static int n=-1;
+    static int random[10];
+    static int vis[100];
+    int rand_percent=10;
+    // choose ten random numbers when k=-1 
+    int i;
+    if(k==-1){
+         for(i=0;i<100;i++) vis[i]=0;
+    }
+    if(k==-1){
+         while(1){
+            k++; 
+            for(i=0;i<100;i++) vis[i]=0; 
+            if(k==rand_percent) break;
+          random:  value = rand()%100;
+            if(vis[value]==0){
+                    vis[value]=1;
+                    random[k]=value;
+                    k++; 
+            }
+            else {goto random;}
+         } 
+    }
+    n++;   
+    if(n==100){
+         k=-1;
+         n=0;
+         for(i=0;i<rand_percent;i++){
+              random[i]=0;
+         }
+         for(i=0;i<100;i++)vis[i]=0;  
+    }
+    for(i=0;i<rand_percent;i++){
+        if(n==random[i]){
+               random[i]=-1;
+               goto rtn;
+        }     
+   }          
+               
     host_to_network(buffer);
     numOfBytesSent = sendto(udp, buffer, strlen(buffer), 0, (struct sockaddr *) &hostAddr, sizeof(struct sockaddr));
 
-  rtn:
+rtn:
     funcExit(logF, ipAddress, "sendUDP", numOfBytesSent);
     return numOfBytesSent; 
 
