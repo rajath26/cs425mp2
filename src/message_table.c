5ed5a13ab5abe7d4a64ab8b81a409a7b17e31975
grkrthk <grkrthk@yahoo.co.in>
2013-10-06 15:25:47 -0500

    comments added to message_table.c

--- src/message_table.c
+++ src/message_table.c
@@ -1,47 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<unistd.h>
 #include"logger.c"
 #include"message.h"
-#define TREMOVE 20 
+/////////////////////////////////////////////////////////////////////////////////////
+//                FILE NAME : message_table.c                                      //
+//                              						   //	
+// Description : This file provides the following functionalities                  //
+//                 * Provides interfaces to serialize and de-serialize messages    //
+//                 * Provides interfaces to convert messages to machine independent//
+//                   forms.						           //		
+//                 * Provides interfaces to perform various operation on a table   //
+//                 * Provides interfaces to update the table, delete an entry and  //
+//                   initialize a table.                                           //
+/////////////////////////////////////////////////////////////////////////////////////
 
-//FILE *log1;
+
+char logMsg1[500];   // Global variable to send the log messages to the logger interface
+
+
+
 /*
-#define UP 1
-#define DOWN 0
-#define MAX_HOSTS 4
-#define TFAIL 10
-#define JOIN_OPCODE 1
-#define LEAVE_OPCODE 2
-
-char *host_ip_address="192.168.100.100";
-char *host_port="1234";
-
-int host_no=0;
-
-struct two_hosts{
-  int host_id;
-  int valid;
-};
-
-struct hb_entry {
-  int  valid;
-  char host_id[70];  // combination of host_id and time_of_creation
-  char IP[80];
-  char port[10];
-  int hb_count;
-  char time_stamp[20];
-  int status;
-}; 
-
-struct hb_entry entry[4];  // this table is used to extract values from the message
-struct hb_entry hb_table[4];  // this is the heart beat table mantained for a single host
-*/
-
-//FILE *log1=fopen("machine1.log", "w");
-
-char logMsg1[500];
+ *  This function is used for deleting an entry in the table
+ *  input param : index of the table that needs to be deleted
+ */
 
 int delete_entry_table(int table_index)
 {
@@ -53,13 +35,16 @@ int delete_entry_table(int table_index)
    return 0;
 }
 
+/*
+ * This function provides functionality to clear the table entries
+ * input param : table which needs to be removed
+ */
+
 int clear_temp_entry_table(struct hb_entry *msg_table)
 {
   funcEntry(logF,ip_Address,"clear_temp_entry_table");
   int i=0;
-  //char message[100];
- // printToLog(log1,hb_table[host_no].host_id,"temporary entry is being deleted");
-   printToLog(logF,ip_Address,"clear temp entry table");
+  printToLog(logF,ip_Address,"clear temp entry table");
   for(i=0;i<MAX_HOSTS;i++){
      memset(&msg_table[i],0,sizeof(struct hb_entry));
   }
@@ -67,19 +52,29 @@ int clear_temp_entry_table(struct hb_entry *msg_table)
   return 0;
 }
 
+/*
+ * This function is used to update the host's heart-beat periodically
+ *
+ */
+
 int update_my_heartbeat()
 {
   funcEntry(logF,ip_Address,"update_my_heart_beat");
   struct timeval timer;
-  char buffer[32];
   pthread_mutex_lock(&table_mutex);
   hb_table[host_no].hb_count++;
   gettimeofday(&timer,NULL);
   sprintf(hb_table[host_no].time_stamp,"%ld",timer.tv_sec);
+  printToLog(logF,"UPDATE MY HB",hb_table[host_no].time_stamp);
   pthread_mutex_unlock(&table_mutex);
   funcExit(logF,ip_Address,"update_my_heart_beat",0);
   return 0;
 }
+
+/*
+ * This function provides functionality to check the table for failed hosts
+ * periodically.
+ */
 
 int check_table_for_failed_hosts()
 { 
@@ -93,14 +88,11 @@ int check_table_for_failed_hosts()
                 long int cmp_time = atoi(hb_table[i].time_stamp);
                 if((timer.tv_sec - cmp_time) >= TFAIL){
                            hb_table[i].status = DOWN;
-                           /*put a logger message*/
-                           //char message[100];
                            sprintf(logMsg1,"Entry %d is being marked DOWN\n",i);
                            printToLog(logF,ip_Address,logMsg1);
                 }
                 if((timer.tv_sec - cmp_time) >= TREMOVE){
                            delete_entry_table(i);
-                           //char message[100];
                            sprintf(logMsg1,"Entry %d is being removed\n",i);  
                            printToLog(logF,ip_Address,logMsg1);
                 }
@@ -112,19 +104,11 @@ int check_table_for_failed_hosts()
 }
 
 /*
-void periodic_heartbeat_update()
-{
-  pthread_mutex_lock(&table_mutex);
-  while(1){
-       sleep(HEART_BEAT_UPDATE_SEC);
-       update_my_entry();
-       check_table_for_failed_hosts();
-  }
-  pthread_mutex_unlock(&table_mutex);
-}        
-*/  
+ *  This function provides functionality to convert messages to platform independent 
+ *  messages. Helps convert to network to host endianness
+ *  input params : message which needs to be converted 
+ */
 
-//network to host and host to network functionality added
 int network_to_host(char *message)
 {
    int i;
@@ -139,6 +123,11 @@ int network_to_host(char *message)
      ptr++;
    }
 }
+
+/*
+ * This function helps convert messages from host to network endianess
+ *
+ */
 int host_to_network(char *message)
 {
    int i;
@@ -152,6 +141,11 @@ int host_to_network(char *message)
       ptr++;
    }
 }
+
+/*
+ * This function helps to initialize the table with appropriate values
+ * input params : port, ip and the host_id
+ */
 
 int initialize_table(char *port,char *ip,int host_id)
 {
@@ -167,11 +161,8 @@ int initialize_table(char *port,char *ip,int host_id)
   printToLog(logF,ip_Address,"Initializing gossip table");
   printToLog(logF,"DEBUG ME",port);
   printToLog(logF,"DEBUg ME",ip);
-  //printToLog(logF,"DEBUG ME", itoa(host_id));
   printToLog(logF,"DEBUG ME",portNo);
   printToLog(logF,"DEBUg ME",ipAddr);
-  //printToLog(logF,"DEBUG ME", itoa(hostId));
-//  pthread_mutex_lock(&table_mutex);
   for(i=0;i<MAX_HOSTS;i++)
     {
       if(i!=host_id){
@@ -195,10 +186,15 @@ int initialize_table(char *port,char *ip,int host_id)
       }
     }
     funcExit(logF,ip_Address,"initialize_table",0);
-//  pthread_mutex_unlock(&table_mutex);
 
 return 0;
 }    
+
+/*
+ * This function provides functionality to serialize the table objects to a message
+ * input params : buffer to which the serialized objects will be populated.
+ *
+ */
 int create_message(char *buffer)
 {
    funcEntry(logF,ip_Address,"create_message");
@@ -216,6 +212,10 @@ int create_message(char *buffer)
    return 0;
 }
 
+/*
+ * This function provides functionality to print the table
+ */
+
 int print_table(struct hb_entry *table)
 {
    int i=0;
@@ -231,12 +231,16 @@ int print_table(struct hb_entry *table)
    return 0;
 }
 
+/*
+ * This interface provides functionality to update the table comparing the time_stamps
+ * in the received message objects
+ */
+
 int update_table(struct hb_entry *msg_table)
 { 
   int i=0;
   funcEntry(logF,ip_Address,"update_table");
   pthread_mutex_lock(&table_mutex);
-  //clear_temp_entry_table(msg_table);
   for(i=0;i<MAX_HOSTS;i++){
        if(msg_table[i].valid && msg_table[i].status){
               if(msg_table[i].hb_count > hb_table[i].hb_count){
@@ -262,18 +266,22 @@ int update_table(struct hb_entry *msg_table)
                          strcpy(hb_table[i].time_stamp,buffer);
                          hb_table[i].status=msg_table[i].status;
                        }
+                       sprintf(logMsg, "%d\t::\t%s\t::\t%s\t::\t%s\t::\t%d\t\t::\t%s\t\t::\t%d\n",hb_table[i].valid,hb_table[i].host_id,hb_table[i].IP,hb_table[i].port,hb_table[i].hb_count,hb_table[i].time_stamp,hb_table[i].status);
               }
        }
   }
   clear_temp_entry_table(msg_table);
   pthread_mutex_unlock(&table_mutex);
- // logMsg=create_message(hb_table);
   printToLog(logF,ip_Address,logMsg1);
   funcExit(logF,ip_Address,"update_table",0);
   return 0;
 }
 
 
+/*
+ * This function provides functionality to deserialize the message received
+ * input params : received message
+ */
 
 struct hb_entry* extract_message(char *input)
 { 
@@ -289,45 +297,34 @@ struct hb_entry* extract_message(char *input)
    while (ptr != NULL)
    {
             strcpy(a[j], ptr);
-            printf("%s\n",ptr);         
             ptr = strtok(NULL, ";");
             j++;
     }
-    printf("no_entries :%d\n",j);
     i=0;
     while(i<j && i<MAX_HOSTS){
-            
-               printf("inside_while : %s\n",a[i]);
-                      
+                                  
                char *token=strtok(a[i],delim);
                entry[i].valid=atoi(token);
-               //  printf("i am here\n");
-               printf("valid: %d\n",entry[i].valid);
+
 	       if(entry[i].valid){    
      	 
         	    token=strtok(NULL,delim);
            	    strcpy(entry[i].host_id,token);
-       	            printf("%s\n",entry[i].host_id);
-
+ 
                     token=strtok(NULL,delim);
                     strcpy(entry[i].IP,token);
-                    printf("%s\n",entry[i].IP);            
   
                     token=strtok(NULL,delim);
                     strcpy(entry[i].port,token);
-                    printf("%s\n",entry[i].port);
  
                     token=strtok(NULL,delim);
                     entry[i].hb_count = atoi(token);
-                    printf("%d\n",entry[i].hb_count); 
             
                     token=strtok(NULL,delim);
                     strcpy(entry[i].time_stamp,token);
-                    printf("%s\n",entry[i].time_stamp);
         
                     token=strtok(NULL,delim);
                     entry[i].status = atoi(token);
-                    printf("%d\n",entry[i].status);
                }
               else{
                  memset(&entry[i],0,sizeof(struct hb_entry));
@@ -346,14 +343,13 @@ void initialize_two_hosts(struct two_hosts* ptr)
    memset(&ptr[1],0,sizeof(struct two_hosts));
 }
 
-/* algorithm for finding two neighboring hosts */
+/* algorithm for finding n neighboring hosts */
 
 int choose_n_hosts(struct two_hosts *ptr, int choice)
 {
   funcEntry(logF,ip_Address,"choose_n_hosts");
   int i;
   int list[MAX_HOSTS];
-  //see who are all present in the hb_table
   int k=0;
   int vis[MAX_HOSTS];
   for(i=0;i<MAX_HOSTS;i++)vis[i]=0;
@@ -366,15 +362,11 @@ int choose_n_hosts(struct two_hosts *ptr, int choice)
                k++;
            }
   }
-
-  if (k==0) 
-{
-funcExit(logF, "", "choose_n_hosts", -1);
-return -1;
-
-}
-  if(k==1)
-  {
+  if (k==0){
+      funcExit(logF, "", "choose_n_hosts", -1);
+      return -1;
+  }
+  if(k==1){
      ptr[0].host_id = list[0];
      ptr[0].valid = 1;
      funcExit(logF, "", "choose_n_hosts", 1);
@@ -399,27 +391,12 @@ return -1;
  return choice;
 }
   
+
 /*
-int choose_two_hosts(struct two_hosts* ptr)
-{
-  int cur_host = host_no;
-  int nbr_host = (host_no+1)%MAX_HOSTS;
-  int i=0;
-  int count=0;
-  pthread_mutex_lock(&table_mutex);
-  for(i=0;i<MAX_HOSTS-1;i++){
-       if(hb_table[nbr_host].valid){
-                 ptr[count].host_id = nbr_host;
-                 ptr[count].valid = 1;
-                 count++;
-        if(count==2) break;
-       }
-       nbr_host=(nbr_host+1)%MAX_HOSTS; 
-  }
-  pthread_mutex_unlock(&table_mutex);
- return count;             
-}  
-*/
+ * This functionality is deprecated and not used anymore by daisy
+ *
+ */
+
 void go_live(char *message)
 {
   char *buffer = (char *)malloc(2000);
